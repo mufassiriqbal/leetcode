c8095ec997bb9ff6e9800fe8757c4fc18609fd95
mufassir <mufassir378@gmail.com>
2025-09-02 16:52:55 +0500

    getting max profit

---
+++ maxProfit.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+       min_price = float("inf")
+       max_price = 0 
+       for price in prices:
+            if price < min_price:
+                min_price =  price
+            elif price - min_price > max_price:
+                max_price = price - min_price
+       return max_price
+
+
+
+
+
+
+        
